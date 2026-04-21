40fc9d41e0999d1e905c76a8fe718d0caeb495b1
Asmita2810 <chaudhariasmita28@gmail.com>
2026-04-21 15:44:20 +0530

    Add files via upload

---
+++ Lab014.py
@@ -0,0 +1,28 @@
+from operator import truediv
+
+print("Hello world")
+#comment
+#Variable
+#Data type in python
+#Int , bool, string, complex,float
+
+#int
+a = 35
+print(a)
+
+#Str
+b = ("Hello")
+print(b)
+
+#float
+c = 3.14
+print(c)
+
+#Bool
+d = True
+print(d)
+
+# complex
+e=  3+4j
+print(e)
+
